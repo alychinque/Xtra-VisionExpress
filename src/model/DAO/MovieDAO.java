1814e31bb6f2d4bd63b94d51b20392910f6137c1
alychinque <alychinque@gmail.com>
2021-04-27 09:59:15 +0100

    Created methods getLastqoMovies and getDescription.

--- src/model/DAO/MovieDAO.java
+++ src/model/DAO/MovieDAO.java
@@ -17,35 +17,83 @@ import model.Movie;
  * @author Alysson Chinque
  */
 public class MovieDAO {
-    
+
     private final Connection connection;
     private Movie movies;
+    ArrayList<Movie> moviesArray = new ArrayList<>();
 
     public MovieDAO(Connection conn) {
         this.connection = conn;
     }
-    
-    //THIS METHOD RETRIEVE ALL MOVIES
-    public ArrayList<Movie> getMovies() throws SQLException{
-        //query to get all movies
-        String query = "SELECT * FROM Alysson_2019305.movie;";
+
+    //THIS METHOD RETRIEVE THE LAST 10 MOVIES
+    public ArrayList<Movie> getLast10Movies() throws SQLException {
+        //query to get the movies
+        String query = "SELECT id_movie, title, subTitle FROM (\n"
+                + "SELECT * FROM Alysson_2019305.movie ORDER BY id_movie DESC LIMIT 10\n"
+                + ")var1\n"
+                + "ORDER BY id_movie ASC;";
         //creating a Statement assigning a connection with the select query
         PreparedStatement stmt = connection.prepareStatement(query);
         stmt.execute();
-        
+
         ResultSet resultSet = stmt.getResultSet();
-        
+
         ArrayList<Movie> moviesArray = new ArrayList<>();
-        
+
         while (resultSet.next()) {
             int id = resultSet.getInt("id_movie");
             String title = resultSet.getString("title");
             String subTitle = resultSet.getString("subTitle");
-            System.out.println("inside while: " +id+ title+subTitle);
             movies = new Movie(id, title, subTitle);
             moviesArray.add(movies);
         }
-        System.out.println("size: "+ moviesArray.size());
         return moviesArray;
+    }
+
+    public ArrayList<Movie> getMovies() throws SQLException {
+        //query to get all movies
+        String query = "SELECT id_movie, title, subTitle FROM Alysson_2019305.movie;";
+        //creating a Statement assigning a connection with the select query
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.execute();
+
+        ResultSet resultSet = stmt.getResultSet();
+
+        ArrayList<Movie> moviesArray = new ArrayList<>();
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id_movie");
+            String title = resultSet.getString("title");
+            String subTitle = resultSet.getString("subTitle");
+            System.out.println("inside while: " + id + title + subTitle);
+            movies = new Movie(id, title, subTitle);
+            moviesArray.add(movies);
+        }
+        System.out.println("size: " + moviesArray.size());
+        return moviesArray;
+    }
+
+    public Movie getDescription(int idMovie) throws SQLException {
+        //query to get all movies
+        String query2 = "SELECT * FROM Alysson_2019305.movie\n"
+                + "where id_movie = ?;";
+        //creating a Statement assigning a connection with the select query
+        PreparedStatement stmt = connection.prepareStatement(query2);
+        stmt.setInt(1, idMovie);
+        stmt.execute();
+
+        ResultSet resultSet = stmt.getResultSet();
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id_movie");
+            String title = resultSet.getString("title");
+            String subTitle = resultSet.getString("subTitle");
+            int idGenre = resultSet.getInt("idgenre");
+            String description = resultSet.getString("description");
+            int duration = resultSet.getInt("duration");
+            movies = new Movie(id, title, subTitle, idGenre, description, duration);
+        }
+        return movies;
     }
 }
