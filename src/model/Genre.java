ef6dea33f3aa6e515a8433eef6bda292d258fd30
alychinque <alychinque@gmail.com>
2021-04-29 10:33:01 +0100

    Created model class Genre.

---
+++ src/model/Genre.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author alych
+ */
+public class Genre {
+    private int idGenre;
+    private String genre;
+
+    public Genre(int idGenre, String genre) {
+        this.idGenre = idGenre;
+        this.genre = genre;
+    }
+}
