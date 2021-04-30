a9edb95680d42cc2a8245db4effc5095f2a31887
alychinque <alychinque@gmail.com>
2021-04-30 19:23:30 +0100

    Rewrite the method backMain.

--- src/controller/CartController.java
+++ src/controller/CartController.java
@@ -6,14 +6,6 @@
 package controller;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import model.DAO.ConnectionDB;
-import model.DAO.MovieDAO;
-import model.Movie;
 import view.Cart;
 import view.Main;
 
@@ -24,27 +16,13 @@ import view.Main;
 public class CartController {
     private final Cart view;
     Connection conn;
-    ArrayList<Movie> moviesArray;
 
     public CartController(Cart view) {
         this.view = view;
     }
 
-    public void backMain() {
-         try {
-             conn = new ConnectionDB().getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
-            JOptionPane.showMessageDialog(null, "Failed");
-        }
-        MovieDAO moviedao = new MovieDAO(conn);
-        try {
-             moviesArray = moviedao.getLast10Movies();
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Failed");
-        }
-        
-        Main main = new Main(moviesArray);
+    public void backMain(int session) {        
+        Main main = new Main(session);
         main.setVisible(true);
         this.view.dispose();
     }
