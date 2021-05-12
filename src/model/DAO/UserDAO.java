1a537258c4868453ab92f81c1757a45c1e98ff53
alychinque <alychinque@gmail.com>
2021-05-12 17:14:14 +0100

    Created method insertUser in the DB UserDAO.

--- src/model/DAO/UserDAO.java
+++ src/model/DAO/UserDAO.java
@@ -6,7 +6,8 @@
 package model.DAO;
 
 import java.sql.Connection;
-
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
@@ -20,5 +21,18 @@ public class UserDAO {
         this.connection = conn;
     }
 
-    
+    public void insertUser(String email, String cardName, String numberCard, int month, int year, String cvc, int session) throws SQLException {
+        String query = "INSERT INTO Alysson_2019305.user (card_number, name_user, month_card, year_card, cvc, session, email_user)\n"
+                + "values (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, numberCard);
+        stmt.setString(2, cardName);
+        stmt.setInt(3, month);
+        stmt.setInt(4, year);
+        stmt.setString(5, cvc);
+        stmt.setInt(6, session);
+        stmt.setString(7, email);
+        stmt.execute();
+    }
+
 }
