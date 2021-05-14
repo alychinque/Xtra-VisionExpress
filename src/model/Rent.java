0b60a11ae04f77d6b6da6cc0ab3b02662eb9d8f3
alychinque <alychinque@gmail.com>
2021-05-14 11:28:43 +0100

    Created model Rent class, constructor, and getters methods.

---
+++ src/model/Rent.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Alysson Chinque
+ */
+public class Rent {
+    private int idUser;
+    private String[] idMovie;
+    private int rentNumber;
+    private String rentDate;
+    private String returnDate;
+    private boolean returned;
+    private float rentCharge;
+    private float extraCharge;
+
+    public Rent(int idUser, String[] idMovie, int rentNumber, String rentDate, String returnDate, boolean returned, float rentCharge, float extraCharge) {
+        this.idUser = idUser;
+        this.idMovie = idMovie;
+        this.rentNumber = rentNumber;
+        this.rentDate = rentDate;
+        this.returnDate = returnDate;
+        this.returned = returned;
+        this.extraCharge = extraCharge;
+    }
+
+    public Rent(int idUser, String[] idMovie, int rentNumber, String rentDate, String returnDate, boolean returned, float rentCharge) {
+        this.idUser = idUser;
+        this.idMovie = idMovie;
+        this.rentNumber = rentNumber;
+        this.rentDate = rentDate;
+        this.returnDate = returnDate;
+        this.returned = returned;
+        this.rentCharge = rentCharge;
+    }
+    
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String[] getIdMovie() {
+        return idMovie;
+    }
+
+    public int getRentNumber() {
+        return rentNumber;
+    }
+
+    public String getRentDate() {
+        return rentDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public boolean isReturned() {
+        return returned;
+    }
+
+    public float getRentCharge() {
+        return rentCharge;
+    }
+    
+}
