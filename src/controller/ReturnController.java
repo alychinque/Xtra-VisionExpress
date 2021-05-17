4bf48234e6a96dd3c1f2d789ee9713ad212c1a9a
alychinque <alychinque@gmail.com>
2021-05-17 21:46:42 +0100

    Created variables in the ReturnController.

--- src/controller/ReturnController.java
+++ src/controller/ReturnController.java
@@ -5,8 +5,18 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
 import model.DAO.ConnectionDB;
 import model.DAO.RentDAO;
 import view.Home;
@@ -19,13 +29,19 @@ import view.Return;
 /**
  * This class will create the return screen.
  */
-public class ReturnController {
+public class ReturnController implements ActionListener {
+
     private final Return view;
     private Connection conn;
     private RentDAO rentdao;
-    private int[] rentNumbers;
-    private ArrayList<String> moviesTitle =  new ArrayList<>();
-    private ArrayList<Integer> rentNumber =  new ArrayList<>();
+    private String rentNumberSelected = "";
+    private String movie = "";
+    private ArrayList<String> moviesTitle = new ArrayList<>();
+    private ArrayList<Integer> rentNumber = new ArrayList<>();
+    private final Calendar cal;
+    private final SimpleDateFormat sdf;
+    private String today;
+    private double fee = 1.50;
 
     public ReturnController(Return view) {
         try {
@@ -34,9 +50,11 @@ public class ReturnController {
         }
         this.view = view;
         rentdao = new RentDAO(conn);
+        cal = new GregorianCalendar();
+        sdf = new SimpleDateFormat("dd/MM/yyyy");
     }
 
-    public void backMain() {
+    public void backHome() {
         Home home = new Home();
         this.view.dispose();
         home.setVisible(true);
@@ -47,19 +65,21 @@ public class ReturnController {
             moviesTitle = rentdao.getMovies();
             return moviesTitle;
         } catch (Exception e) {
-            
+
         }
         return moviesTitle = null;
     }
 
     public ArrayList<Integer> getRentNumbers() {
-        try{
+        try {
             rentNumber = rentdao.getRentNumbers();
             return rentNumber;
         } catch (Exception e) {
-            
+
         }
         return rentNumber = null;
     }
-    
+
+   
+
 }
